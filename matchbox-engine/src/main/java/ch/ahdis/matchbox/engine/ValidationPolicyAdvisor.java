84ef318f9e534710ee1d47652eb3801a35522ad2
Edmond Chiu <dev@edmondchiu.ca>
2025-01-21 08:27:10 -0500

    remove matchbox implementation for policyForReference

--- matchbox-engine/src/main/java/ch/ahdis/matchbox/engine/ValidationPolicyAdvisor.java
+++ matchbox-engine/src/main/java/ch/ahdis/matchbox/engine/ValidationPolicyAdvisor.java
@@ -1,10 +1,8 @@
 package ch.ahdis.matchbox.engine;
 
 
-import org.hl7.fhir.r5.utils.validation.IResourceValidator;
 import org.hl7.fhir.r5.utils.validation.constants.ReferenceValidationPolicy;
 import org.hl7.fhir.validation.instance.advisor.BasePolicyAdvisorForFullValidation;
-import org.hl7.fhir.validation.instance.InstanceValidator;
 
 public class ValidationPolicyAdvisor extends BasePolicyAdvisorForFullValidation {
 
@@ -12,17 +10,6 @@ public class ValidationPolicyAdvisor extends BasePolicyAdvisorForFullValidation 
         super(refpol);
     }
 
-    @Override
-    public ReferenceValidationPolicy policyForReference(IResourceValidator validator, Object appContext, String path, String url) {
-        if (url!=null && (url.startsWith("http://") || url.startsWith("https://") || url.startsWith("urn:"))) {
-            if (validator instanceof InstanceValidator && ((InstanceValidator)validator).isAllowExamples() && (url.contains("example.org") || url.contains("acme.com") || url.contains("acme.org"))) {
-                return ReferenceValidationPolicy.IGNORE;               
-            }
-            return ReferenceValidationPolicy.CHECK_EXISTS_AND_TYPE;
-        }                                                        
-        return ReferenceValidationPolicy.IGNORE;
-    }
-    
     @Override
     public boolean isSuppressMessageId(String path, String messageId) {
       return false;
